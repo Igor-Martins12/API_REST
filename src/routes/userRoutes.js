5fcc586c2b651ede232e0a2727b8b0920e2bac7a
Igor Martins <igorjose07@hotmail.com>
2023-10-12 22:43:40 -0300

    'atualizandoRoutes'

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,12 +3,12 @@ import userController from "../controllers/UserController";
 import loginRequired from "../middlewares/loginRequired";
 
 const router = new Router();
-router.get('/', userController.index);
+// router.get('/', userController.index);
+// router.get('/:id',loginRequired, userController.show);
 
-router.get('/:id',loginRequired, userController.show);
 router.post('/', userController.store);
-router.put('/:id',  userController.update);
-router.delete('/:id', userController.delete);
+router.put('/', loginRequired,  userController.update);
+router.delete('/', loginRequired, userController.delete);
 
 
 
